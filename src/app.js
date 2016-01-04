54b0f54a292092f30e8be3f45f4035962c89aa2b
tongrhj <tongrhj@gmail.com>
2016-01-04 11:20:54 +0800

    Removed restrictions on homescreen show

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-addToHomescreen()
+const addtohome = addToHomescreen({
+   skipFirstVisit: false,
+   maxDisplayCount: 0,
+   autostart: false,
+   startDelay: 0.1,
+   onShow: function() {
+     console.log('add to homescreen showing')
+   },
+   debug: true
+})
+
+const installBtn = document.querySelector('#install-btn')
+
+installBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  console.log('Install Button Clicked')
+  addtohome.show(true)
+})
